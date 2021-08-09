da3e417eeecea2eeeb48683787a3a99882a7df02
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-08-09 00:48:59 -0400

    Random quote implemented successfully

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,17 @@
 class QuotesController < ApplicationController
   def index
-    quotes = Quote.all
-    render json: { status: 200, quote: quotes }
+    # quotes = Quote.all
+    # render json: { status: 200, quote: quotes }
+    quote = Quote.all.sample
+    render json: { status: 200, quote: quote }
   end
 
   def show
     quote = Quote.find(params[:id])
     render json: {status: 200, quote: quote}
+  end
+
+  def randomquote
+    render json: Quote.all.sample
   end
 end
